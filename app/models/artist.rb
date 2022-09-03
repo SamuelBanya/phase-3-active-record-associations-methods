b132b7120fc139131c23bbc7926e14594a309fe1
Samuel Banya <sbanya@fastmail.com>
2022-09-03 12:02:26 -0400

    commit tree d60ec918d3091ce0b931635db4b91e0dbf7b4f4f

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,15 +2,25 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    # NOTE: Here we are using the #songs instance method that was created by the
+    # 'has_many' macro, and we are using chaining since we will have an array returned
+    # and want to only observe the first result of it with .first():
+    self.songs.first()
+  end
+
+  # return the genre of the artist's first saved song
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    self.songs.first.genre()
   end
 
+  # return the number of songs associated with the artist
   def song_count
-    #return the number of songs associated with the artist
+    self.songs.count()
   end
 
+  # return the number of genres associated with the artist
   def genre_count
-    #return the number of genres associated with the artist
+    self.genres.count()
   end
 end
